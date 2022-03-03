1ea75e2c7854b05d2ba678f827918d32d4d4ff11
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2022-03-03 14:39:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,58 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    radio.sendNumber(0)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Silly)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.clearScreen()
+})
+radio.setGroup(1)
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
 basic.forever(function () {
-	
+    while (input.pinIsPressed(TouchPin.P0) && input.soundLevel() >= 120) {
+        basic.showIcon(IconNames.EigthNote)
+        soundExpression.hello.playUntilDone()
+        basic.pause(500)
+        basic.clearScreen()
+    }
 })
